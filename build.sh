b4c1a6c78bfbfee07951d5d91ce360b619fa5d6b
Arthur Semenyutin <semenyutin@gmail.com>
2021-10-24 19:21:09 +1100

    Provide tags for docker image, remove build workflow

--- build.sh
+++ build.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-SWIFT_FORMAT_VERSION=$(cat swiftformat-version)
+SWIFT_FORMAT_VERSION=$1
 
-# Seems that there are no official Swift images for linux/arm64 platform
+if [ -z "$SWIFT_FORMAT_VERSION" ]; then
+    SWIFT_FORMAT_VERSION=$(
+        curl --silent \
+            -H "Accept: application/vnd.github.v3+json" \
+            https://api.github.com/repos/nicklockwood/SwiftFormat/releases/latest |
+            jq -r .tag_name
+    )
+fi
+
+echo "Building with SwiftFormat:$SWIFT_FORMAT_VERSION..."
+
+# There are no official Swift images for linux/arm64 platform
 # https://hub.docker.com/_/swift
 docker build \
---platform linux/amd64 \
---build-arg swift_format_version=$SWIFT_FORMAT_VERSION \
--t semenyutin/swiftformat:$SWIFT_FORMAT_VERSION \
-.
+    --platform linux/amd64 \
+    --build-arg swift_format_version=$SWIFT_FORMAT_VERSION \
+    -t semenyutin/swiftformat:$SWIFT_FORMAT_VERSION \
+    .
